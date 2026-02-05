6c757f42ec14682801213044aab295cb6e40e142
Andy Harrison <695638+wizard04wsu@users.noreply.github.com>
2026-02-05 09:29:35 -0500

    adopt CSS instead of copy

--- src/element.ts
+++ src/element.ts
@@ -1,23 +1,42 @@
 import template from './template.html?raw';
 import compiledCSS from './styles.less?inline';
 
+const styleSheet = new CSSStyleSheet();
+styleSheet.replaceSync(compiledCSS);
+
 class MyElement extends HTMLElement {
 	
 	static tagName = 'my-element';
 	
 	constructor() {
+		
 		super();
+		
 		this.attachShadow({ mode: 'open' });
+		
+		// Adopt the skeleton CSS.
+		this.shadowRoot!.adoptedStyleSheets.push(styleSheet);
+		
+		// Add the skeleton HTML.
+		this.shadowRoot!.innerHTML = template;
 	}
 	
 	connectedCallback() {
+		// A lifecycle callback called each time the element is inserted into the DOM.
+		
 		// Avoid re-rendering if reconnected
 		if (this.shadowRoot && this.shadowRoot.childNodes.length > 0) return;
+	}
+	
+	disconnectedCallback() {
+		// A lifecycle callback called each time the element is removed from the DOM.
 		
-		this.shadowRoot!.innerHTML = `
-			<style>${compiledCSS}</style>
-			${template}
-		`;
+	}
+	
+	connectedMoveCallback() {
+		// A lifecycle callback called each time the element is moved to a different place in the DOM.
+		// Note that a move is not a disconnect followed by a connect.
+		
 	}
 }
 
